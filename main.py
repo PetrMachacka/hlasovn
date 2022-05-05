890072112711d2de9e67cc0124f046086df7953b
Petr Machačka <91247837+PetrMachacka@users.noreply.github.com>
2022-05-05 22:41:50 +0200

    jestesave

--- main.py
+++ main.py
@@ -4,41 +4,73 @@ radio.set_transmit_power(7)
 radio.set_transmit_serial_number(True)
 start = 0
 cislo = 1
-data_list = {}
+data_list = [[0],[0]]
+myserial = 
+role = "klient"
+basic.show_string("K")
+# Role changing
+def on_logo_event_pressed2():
+    global role
+    if role == "klient": 
+        role = "server"
+        basic.show_string("S")
+    else: 
+        role = "klient"
+        basic.show_string("K")
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed2)
+# Klient
 
 # Moving
 def on_button_pressed_a():
-    global cislo
-    cislo = Math.constrain(cislo, 2, 5)
-    cislo -= 1
-    basic.show_string(String.from_char_code(cislo + 64))
-def on_button_pressed_b():
-    global cislo
-    cislo = Math.constrain(cislo, 2, 5)
-    cislo += 1
-    basic.show_string(String.from_char_code(cislo + 64))
-def on_pin_pressed_p0():
-    radio.send_value("key", cislo)
-input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
-
-# Start/Stop
-def on_logo_event_pressed():
-    global start
-    if start == 1:start = 0
-    else: start = 1
-    
-    radio.send_value("vote", start)
-def on_received_value(name, value):
-    remote_serial = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
-    global start
-    if name ==  "vote":
-        start = value
+    if role == "klient":
+        global cislo
+        cislo = Math.constrain(cislo, 2, 5)
+        cislo -= 1
+        basic.show_string(String.from_char_code(cislo + 64))
+    else:
+        global start
+        if start == 1:start = 0
+        else: start = 1
         print(start)
-    if start == 1 and name == "key":
-        music.play_tone(Note.C, 200)
-        print(remote_serial)
-        print(String.from_char_code(value + 64 ))
+        radio.send_value("vote", start)
+def on_button_pressed_b():
+    if role == "klient":
+        global cislo
+        cislo = Math.constrain(cislo, 2, 5)
+        cislo += 1
+        basic.show_string(String.from_char_code(cislo + 64))
+# Send number
+def on_pin_pressed_p1():
+    if role == "klient":
+        radio.send_value("key", cislo)
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+def on_received_value_klient(name, value):
+    if role == "klient":
+        if name == "acr":
+            music.play_tone(Note.C, 300)
+    if name == "vote":
+        if value == 1:
+            basic.show_icon(IconNames.YES)
+        else:
+            basic.show_icon(IconNames.NO)
 radio.on_received_value(on_received_value)
-input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+# Server
+def on_received_value(name, value):
+    serial_number = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+    global start
+    if role == "server":
+        if start == 1 and name == "key":
+            music.play_tone(Note.C, 200)
+            print(String.from_char_code(value + 64 ))
+            radio.send_value("acr", serial_number)
+    if role == "klient":
+        if name == "acr" and value == control.device_serial_number():
+                music.play_tone(Note.C, 300)
+        if name == "vote":
+            if value == 1:
+                basic.show_icon(IconNames.YES)
+            else:
+                basic.show_icon(IconNames.NO)
+radio.on_received_value(on_received_value)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -4,49 +4,108 @@ radio.setTransmitPower(7)
 radio.setTransmitSerialNumber(true)
 let start = 0
 let cislo = 1
-let data_list =  {
-	
-}
-
-//  Moving
-input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
-    radio.sendValue("key", cislo)
+let data_list = [[0], [0]]
+let myserial = []
+let role = "klient"
+basic.showString("K")
+//  Role changing
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed2() {
+    
+    if (role == "klient") {
+        role = "server"
+        basic.showString("S")
+    } else {
+        role = "klient"
+        basic.showString("K")
+    }
+    
 })
-//  Start/Stop
-radio.onReceivedValue(function on_received_value(name: string, value: number) {
-    let remote_serial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+//  Klient
+//  Moving
+//  Send number
+input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
+    if (role == "klient") {
+        radio.sendValue("key", cislo)
+    }
+    
+})
+function on_received_value_klient(name: any, value: any) {
+    if (role == "klient") {
+        if (name == "acr") {
+            music.playTone(Note.C, 300)
+        }
+        
+    }
     
     if (name == "vote") {
-        start = value
-        console.log(start)
+        if (value == 1) {
+            basic.showIcon(IconNames.Yes)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+        
     }
     
-    if (start == 1 && name == "key") {
-        music.playTone(Note.C, 200)
-        console.log(remote_serial)
-        console.log(String.fromCharCode(value + 64))
+}
+
+radio.onReceivedValue(on_received_value)
+//  Server
+function on_received_value(name: string, value: number) {
+    let serial_number = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    
+    if (role == "server") {
+        if (start == 1 && name == "key") {
+            music.playTone(Note.C, 200)
+            console.log(String.fromCharCode(value + 64))
+            radio.sendValue("acr", serial_number)
+        }
+        
     }
     
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
-    
-    if (start == 1) {
-        start = 0
-    } else {
-        start = 1
+    if (role == "klient") {
+        if (name == "acr" && value == control.deviceSerialNumber()) {
+            music.playTone(Note.C, 300)
+        }
+        
+        if (name == "vote") {
+            if (value == 1) {
+                basic.showIcon(IconNames.Yes)
+            } else {
+                basic.showIcon(IconNames.No)
+            }
+            
+        }
+        
     }
     
-    radio.sendValue("vote", start)
-})
+}
+
+radio.onReceivedValue(on_received_value)
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    if (role == "klient") {
+        
+        cislo = Math.constrain(cislo, 2, 5)
+        cislo -= 1
+        basic.showString(String.fromCharCode(cislo + 64))
+    } else {
+        
+        if (start == 1) {
+            start = 0
+        } else {
+            start = 1
+        }
+        
+        console.log(start)
+        radio.sendValue("vote", start)
+    }
     
-    cislo = Math.constrain(cislo, 2, 5)
-    cislo -= 1
-    basic.showString(String.fromCharCode(cislo + 64))
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    if (role == "klient") {
+        
+        cislo = Math.constrain(cislo, 2, 5)
+        cislo += 1
+        basic.showString(String.fromCharCode(cislo + 64))
+    }
     
-    cislo = Math.constrain(cislo, 2, 5)
-    cislo += 1
-    basic.showString(String.fromCharCode(cislo + 64))
 })
